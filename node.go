07210a7c47721554f1092e1e8156355728a36f4d
JLeliaert <jonathan.leliaert@gmail.com>
2013-12-10 13:36:56 +0100

    com calculated

--- node.go
+++ node.go
@@ -27,10 +27,19 @@ func (n *node) add(p *Particle) {
 	n.lijst = append(n.lijst, p)
 }
 
-func (n node) calculatecom() {
-	//TODO
-}
+func (n *node) calculatecom() {
+	//TODO: hiermee oppassen als de deeltjes een verschillende grootte kunnen hebben
+	comx := 0.
+	comy := 0.
+	comz := 0.
 
+	for i := range n.lijst {
+		comx += n.lijst[i].X
+		comy += n.lijst[i].Y
+		comz += n.lijst[i].Z
+	}
+	n.com = Vector{comx / n.number, comy / n.number, comz / n.number}
+}
 
 func (w node) descend() {
 	w.calculatecom()
@@ -41,58 +50,50 @@ func (w node) descend() {
 		//tlb
 		pos := Vector{w.origin[0] - w.diameter/4, w.origin[1] + w.diameter/4, w.origin[2] + w.diameter/4}
 		w.tlb = new(node)
-		w.tlb.origin=pos
-		w.tlb.diameter=w.diameter/2
-
+		w.tlb.origin = pos
+		w.tlb.diameter = w.diameter / 2
 
 		//tlf
 		pos = Vector{w.origin[0] - w.diameter/4, w.origin[1] + w.diameter/4, w.origin[2] - w.diameter/4}
 		w.tlf = new(node)
-		w.tlf.origin=pos
-		w.tlf.diameter=w.diameter/2
-
+		w.tlf.origin = pos
+		w.tlf.diameter = w.diameter / 2
 
 		//trb
 		pos = Vector{w.origin[0] + w.diameter/4, w.origin[1] + w.diameter/4, w.origin[2] + w.diameter/4}
 		w.trb = new(node)
-		w.trb.origin=pos
-		w.trb.diameter=w.diameter/2
-
+		w.trb.origin = pos
+		w.trb.diameter = w.diameter / 2
 
 		//trf
 		pos = Vector{w.origin[0] + w.diameter/4, w.origin[1] + w.diameter/4, w.origin[2] - w.diameter/4}
 		w.trf = new(node)
-		w.trf.origin=pos
-		w.trf.diameter=w.diameter/2
-
+		w.trf.origin = pos
+		w.trf.diameter = w.diameter / 2
 
 		//blb
 		pos = Vector{w.origin[0] - w.diameter/4, w.origin[1] - w.diameter/4, w.origin[2] + w.diameter/4}
 		w.blb = new(node)
-		w.blb.origin=pos
-		w.blb.diameter=w.diameter/2
-
+		w.blb.origin = pos
+		w.blb.diameter = w.diameter / 2
 
 		//blf
 		pos = Vector{w.origin[0] - w.diameter/4, w.origin[1] - w.diameter/4, w.origin[2] - w.diameter/4}
 		w.blf = new(node)
-		w.blf.origin=pos
-		w.blf.diameter=w.diameter/2
-
+		w.blf.origin = pos
+		w.blf.diameter = w.diameter / 2
 
 		//brb
 		pos = Vector{w.origin[0] + w.diameter/4, w.origin[1] - w.diameter/4, w.origin[2] + w.diameter/4}
 		w.brb = new(node)
-		w.brb.origin=pos
-		w.brb.diameter=w.diameter/2
-
+		w.brb.origin = pos
+		w.brb.diameter = w.diameter / 2
 
 		//brf
 		pos = Vector{w.origin[0] + w.diameter/4, w.origin[1] - w.diameter/4, w.origin[2] - w.diameter/4}
 		w.brf = new(node)
-		w.brf.origin=pos
-		w.brf.diameter=w.diameter/2
-
+		w.brf.origin = pos
+		w.brf.diameter = w.diameter / 2
 
 		//for alle particles in node
 		for i := range w.lijst {
@@ -101,41 +102,41 @@ func (w node) descend() {
 			case 0:
 				w.tlb.number += 1
 				w.tlb.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at tlb")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at tlb")
 			case 1:
 				w.tlf.number += 1
 				w.tlf.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at tlf")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at tlf")
 
 			case 2:
 				w.trb.number += 1
 				w.trb.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at trb")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at trb")
 
 			case 3:
 				w.trf.number += 1
 				w.trf.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at trf")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at trf")
 
 			case 4:
 				w.blb.number += 1
 				w.blb.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at blb")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at blb")
 
 			case 5:
 				w.blf.number += 1
 				w.blf.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at blf")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at blf")
 
 			case 6:
 				w.brb.number += 1
 				w.brb.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at brb")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at brb")
 
 			case 7:
 				w.brf.number += 1
 				w.brf.add(w.lijst[i])
-//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at brf")
+				//fmt.Println("particle at ",Vector{w.lijst[i].X, w.lijst[i].Y, w.lijst[i].Z},"  was put at brf")
 			}
 		}
 		//for iedere subnode
@@ -172,7 +173,7 @@ func Maketree() {
 	//werkt dit??
 	for i := range Lijst {
 		Universe.lijst = append(Universe.lijst, &Lijst[i])
-		Universe.number +=1
+		Universe.number += 1
 	}
 	Universe.descend()
 }
