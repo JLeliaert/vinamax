3dc359bdf19f9d18c97923cfb573434a696ad4c0
JLeliaert <jonathan.leliaert@gmail.com>
2013-12-10 13:48:06 +0100

    added welcome message

--- const.go
+++ const.go
@@ -4,6 +4,7 @@ package vinamax
 
 import (
 	"math"
+	"fmt"
 )
 
 const (
@@ -18,3 +19,26 @@ const (
 	kb     = 1.380650424E-23    // Boltzmann's constant in J/K
 	qe     = 1.60217646E-19     // Electron charge in C
 )
+
+
+func init (){
+
+
+fmt.Println("vinamax: a macrospin model to simulate magnetic nanoparticles")
+fmt.Println("Copyright (C) 2013  Jonathan Leliaert")
+fmt.Println()
+fmt.Println("This program is free software: you can redistribute it and/or modify")
+fmt.Println("it under the terms of the GNU General Public License as published by")
+fmt.Println("the Free Software Foundation, version 3 of the License")
+fmt.Println()
+fmt.Println("This program is distributed in the hope that it will be useful,")
+fmt.Println("but WITHOUT ANY WARRANTY; without even the implied warranty of")
+fmt.Println("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the")
+fmt.Println("GNU General Public License for more details.")
+fmt.Println()
+fmt.Println("You should have received a copy of the GNU General Public License")
+fmt.Println("along with this program.  If not, see [http:fmt.Println(www.gnu.org/licenses/].")
+fmt.Println()
+fmt.Println("contact: jonathan dot leliaert at gmail dot com")
+fmt.Println()
+}
