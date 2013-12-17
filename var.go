2d00a828030f322cdd17cccdfd94db8917b0924d
JLeliaert <jonathan.leliaert@gmail.com>
2013-12-17 14:22:50 +0100

    tests var for weird values

--- var.go
+++ var.go
@@ -1,15 +1,34 @@
 package vinamax
 
+import(
+	"log"
+)
+
 var (
 	B_ext         Vector             // External applied field in T
-	Dt            float64            // Timestep in s
+	Dt            float64   =1e-15         // Timestep in s, default is 1e-15
 	Lijst         Particles          // List containing all the Particles
 	T             float64            // Time in s
-	Alpha         float64            // Gilbert damping constant
+	Alpha         float64  =0.02          // Gilbert damping constant, default=0.02
 	Temp          float64            // Temperature in K
-	Ku1           float64            // Uniaxial anisotropy constant in J/m**3
-	Msat          float64   = 860000 // Saturation magnetisation in A/m
+	Ku1           float64    =0        // Uniaxial anisotropy constant in J/m**3, default is 0
+	Msat          float64   = 860000 // Saturation magnetisation in A/m, default = 860e3
 	Thresholdbeta float64   =0.7            //The threshold value for the FMM
 	Universe      node               // The entire universe of the simulation
 	FMM	      bool 	=true	 // Calculate demag with FMM method
 )
+
+func testinput(){
+if Dt <0{
+log.Fatal("Dt cannot be smaller than 0")
+}
+if Alpha <0{
+log.Fatal("Alpha cannot be smaller than 0")
+}
+if Temp <0{
+log.Fatal("Temp cannot be smaller than 0")
+}
+if Msat <0{
+log.Fatal("Msat cannot be smaller than 0")
+}
+}
