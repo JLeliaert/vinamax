e75f0691f07b4ce79eb7e1eb9024e72e1bd3e48b
JLeliaert <jonathan.leliaert@gmail.com>
2013-12-19 11:08:11 +0100

    temperature looks at volume

--- const.go
+++ const.go
@@ -9,9 +9,9 @@ import (
 
 const (
 	//are these actually const? maybe as Particle property
-	Dx = 2e-9 // Size-x of Particle in m
-	Dy = 2e-9 // Size-y of Particle in m
-	Dz = 2e-9 // Size-z of Particle in m
+//	Dx = 2e-9 // Size-x of Particle in m
+//	Dy = 2e-9 // Size-y of Particle in m
+//	Dz = 2e-9 // Size-z of Particle in m
 
 	gamma0 = 1.7595e11          // Gyromagnetic ratio of electron, in rad/Ts
 	mu0    = 4 * math.Pi * 1e-7 // Permeability of vacuum in Tm/A
