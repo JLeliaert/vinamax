3e97640650c2cf52df85905ea68c423d0204454a
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-01-09 10:48:10 +0100

    B_eff.go: *Particle receivers: 10% speed boost

--- B_eff.go
+++ B_eff.go
@@ -6,7 +6,7 @@ import (
 )
 
 //Sums the individual fields to the effective field working on the Particle
-func (p Particle) b_eff() Vector {
+func (p *Particle) b_eff() Vector {
 	return p.demagnetising_field.add(p.anis().add(p.zeeman().add(p.temp())))
 }
 
@@ -15,15 +15,16 @@ func SetRandomSeed(a int64) {
 	rand.Seed(a)
 }
 
+var rng = rand.New(rand.NewSource(0))
+
 //Calculates the the thermal field B_therm working on a Particle
 // TODO factor 4/3pi in "number" omdat ze bol zijn!
-func (p Particle) temp() Vector {
+func (p *Particle) temp() Vector {
 	B_therm := Vector{0., 0., 0.}
 	if Temp != 0. {
-
-		etax := rand.NormFloat64()
-		etay := rand.NormFloat64()
-		etaz := rand.NormFloat64()
+		etax := rng.NormFloat64()
+		etay := rng.NormFloat64()
+		etaz := rng.NormFloat64()
 		B_therm = Vector{etax, etay, etaz}
 		number := math.Sqrt((2 * kb * Alpha * Temp) / (gamma0 * p.msat * 4. / 3. * math.Pi * math.Pow(p.r, 3) * Dt))
 		B_therm = B_therm.times(number)
@@ -32,12 +33,12 @@ func (p Particle) temp() Vector {
 }
 
 //Calculates the zeeman field working on a Particle
-func (p Particle) zeeman() Vector {
+func (p *Particle) zeeman() Vector {
 	return B_ext
 }
 
 //Calculates the anisotropy field working on a Particle
-func (p Particle) anis() Vector {
+func (p *Particle) anis() Vector {
 	//2*Ku1*(m.u)*u/p.msat
 	var m Vector
 	m[0] = p.m[0]
