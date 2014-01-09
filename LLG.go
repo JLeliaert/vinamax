e7fa24095a6566b91086511a3641700b56607ba0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-01-09 11:51:03 +0100

    tau minor tweak

--- LLG.go
+++ LLG.go
@@ -3,9 +3,7 @@ package vinamax
 //Calculates the torque working on the magnisation of a Particle
 func (p Particle) tau() Vector {
 	mxB := p.m.cross(p.b_eff())
-	mxmxB := p.m.cross(mxB)
-	mxmxB = mxmxB.times(Alpha)
-	mxB = mxB.add(mxmxB)
-	tau := mxB.times(1 / (1 + Alpha*Alpha))
-	return tau.times(-gamma0)
+	amxmxB := p.m.cross(mxB).times(Alpha)
+	mxB = mxB.add(amxmxB)
+	return mxB.times(-gamma0 / (1 + Alpha*Alpha))
 }
