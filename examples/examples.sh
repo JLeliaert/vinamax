c796c5ad0ac57dae97d90f8bf06e625c9cb6ee09
JLeliaert <jonathan.leliaert@gmail.com>
2014-01-25 14:39:31 +0100

    update examples.sh

--- examples/examples.sh
+++ examples/examples.sh
@@ -3,4 +3,6 @@ go run example1.go
 gnuplot example1.gnu
 epstopdf example1.eps
 rm example1.eps
-convert -verbose -density 125 -trim example1.pdf -quality 100 -sharpen 0x1.0 example1.png
+
+#only to update the figure for the webpage
+#convert -verbose -density 125 -trim example1.pdf -quality 100 -sharpen 0x1.0 example1.png
