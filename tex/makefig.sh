62cf152db4740bef4ee8806bf2af41eff50986cd
JLeliaert <jonathan.leliaert@gmail.com>
2014-02-16 13:27:04 +0100

    fixed formula

--- tex/makefig.sh
+++ tex/makefig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make
 #only to update the figures for the webpage
-#convert -verbose -density 125 -trim vinamax.pdf -quality 100 -sharpen 0x1.0 vinamax.png
+convert -verbose -density 125 -trim vinamax.pdf -quality 100 -sharpen 0x1.0 vinamax.png
  
 make clean
 
