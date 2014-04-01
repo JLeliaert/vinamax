a3e190391280048767c2bf807d2b3fd7b3c09995
JLeliaert <jonathan.leliaert@gmail.com>
2014-04-01 17:29:15 +0200

    added jumpnoise file

---
+++ jumpnoise.go
@@ -0,0 +1,18 @@
+//This file contains functions used for the jumpnoise
+
+package vinamax
+
+import (
+	"math"
+	//	"math/rand"
+)
+
+//calculates the attempt frequency of a particle
+func attemptf(p particle) float64 {
+	prefactor := Alpha * gamma0 * mu0 / (1 + Alpha*Alpha)
+	volume := cube(p.r) * 4 / 3. * math.Pi
+	anisotropyfield := size(p.anis()) / mu0
+	undersqrt := cube(anisotropyfield) * p.msat * volume / (2 * math.Pi * kb * Temp)
+
+	return prefactor * math.Sqrt(undersqrt)
+}
