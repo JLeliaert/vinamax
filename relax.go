2d79672d2f3edebb9ee64b90eac9fe01b2658e3e
JLeliaert <jonathan.leliaert@gmail.com>
2019-03-18 16:12:07 +0100

    improved relax function

--- relax.go
+++ relax.go
@@ -6,50 +6,52 @@ import (
 	"math"
 )
 
-
-
 func Relax() {
+	backuptol := Errortolerance
+	backuptime := T
+	backupdt := Dt
 
-			gammaoveralpha = gamma0 / (1. + (Alpha * Alpha))
-				relax=true
-if Demag {
-					calculatedemag()
-				}
-				dopristep(Universe.lijst)
+	gammaoveralpha = gamma0 / (1. + (Alpha * Alpha))
+	relax = true
+	if Demag {
+		calculatedemag()
+	}
+	dopristep(Universe.lijst)
+	Errortolerance=1e-3
+		for maxtauwitht > 1e-10 {
 
-		for maxtauwitht>1e-10{
+		if Demag {
+			calculatedemag()
+		}
+		dopristep(Universe.lijst)
 
-				if Demag {
-					calculatedemag()
-				}
-				dopristep(Universe.lijst)
+		if maxtauwitht > Errortolerance {
+			undobadstep(Universe.lijst)
+			if BrownianRotation {
+				undobadstep_u_anis(Universe.lijst)
+			}
+			if Dt == Mindt {
+				log.Fatal("mindt is too small for your specified error tolerance")
+			}
+		}
 
-				//fmt.Println(Dt)
-				if Adaptivestep {
-					if maxtauwitht > Errortolerance {
-						undobadstep(Universe.lijst)
-						if BrownianRotation {
-							undobadstep_u_anis(Universe.lijst)
-						}
-						if Dt == Mindt {
-							log.Fatal("mindt is too small for your specified error tolerance")
-						}
-					}
+		Dt = 0.95 * Dt * math.Pow(Errortolerance/maxtauwitht, (1./float64(order)))
 
-					Dt = 0.95 * Dt * math.Pow(Errortolerance/maxtauwitht, (1./float64(order)))
-
-					if Dt < Mindt {
-						Dt = Mindt
-					}
-					if Dt > Maxdt {
-						Dt = Maxdt
-					}
-					//fmt.Println("dt:   ", Dt)
-					//maxtauwitht = 1.e-12
-				}
+		if Dt < Mindt {
+			Dt = Mindt
+		}
+		if Dt > Maxdt {
+			Dt = Maxdt
+		}
+		if maxtauwitht < Errortolerance/16{
+			Errortolerance/=1.44
+		}
+		T=backuptime
 	}
 
-
-	relax=false
+	Errortolerance = backuptol
+	T=backuptime
+	Dt=backupdt
+	relax = false
 
 }
